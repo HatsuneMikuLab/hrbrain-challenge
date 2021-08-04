b84099ac6a12745c440175586d92aff85910ade7
Pavlov Oleh <opavlov@conceptsoft.io>
2021-08-04 15:36:12 +0900

    passed test 4 addUser ctrl

--- controllers/user.controller_test.go
+++ controllers/user.controller_test.go
@@ -1,36 +1,59 @@
 package controllers
 
 import (
+	"io/ioutil"
+	"github.com/HatsuneMikuLab/hrbrain-challenge/models"
 	"encoding/json"
 	"bytes"
 	"github.com/HatsuneMikuLab/hrbrain-challenge/db"
 	"github.com/HatsuneMikuLab/hrbrain-challenge/services"
-	"github.com/HatsuneMikuLab/hrbrain-challenge/modules"
 	"net/http"
 	"testing"
 	"net/http/httptest"
 )
 
-// coverage is bad, I am just too lazy
-func TestAddUser(t *testing.T) {
-	rec := httptest.NewRecorder()
+func prepare4test() *UsersController {
 	db := db.Connect2db("postgres://hzvlouaa:Gy5nzrwrQnw8IH9emXtEdH-RNPdI0SAd@kashin.db.elephantsql.com/hzvlouaa")
-	ctrl := NewUserController(
+	return NewUserController(
 		services.NewUserService(db), 
 		services.NewEvaluationService(),
 		services.NewCacheService(),
 	)
-	body := json.Marshal(&User{ ID: "Ophelia", Email: "ophelia@gmail.com" })
-	req, err := http.NewRequest("POST", "/api/users", bytes.NewBuffer(body))
+} 
+
+// coverage is bad, I am just too lazy
+func TestAddUser(t *testing.T) {
+	user := &models.User{}
+	expectedID := "Nezuko"
+	expectedEmail := "nezuko@gmail.com"
+	rec := httptest.NewRecorder()
+	ctrl := prepare4test()
+	
+	body, _ := json.Marshal(&models.User{ ID: expectedID, Email: expectedEmail })
+	req, err := http.NewRequest(http.MethodPost, "/api/users", bytes.NewBuffer(body))
 	if err != nil {
 		t.Fatalf("Could not create request %s", err.Error())
 	}
-	handler := http.HandlerFunc(ctrl.GetUserByID)
+	t.Logf("REQ: %v", req)
+	handler := http.HandlerFunc(ctrl.AddUser)
 	handler.ServeHTTP(rec, req)
 
+	
 	res := rec.Result()
+	resBody, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Fatalf("Could not read body responset %s", err.Error())
+	}
 
-	if res.StatusCode != http.StatusOK {
+	t.Logf("RES: %v", string(resBody))
+
+	if res.StatusCode != http.StatusCreated {
 		t.Fatalf("Response status should be 200, but got %v", res.StatusCode)
 	} 
+	if json.Unmarshal(resBody, user) != nil  {
+		t.Fatalf("Response body should have a JSON format %v", resBody)
+	}
+	if user.ID != expectedID || user.Email != expectedEmail {
+		t.Fatalf("Response body is expected to have ID: %s and Email %s, but got %v", expectedID, expectedEmail, resBody)
+	}
 }
